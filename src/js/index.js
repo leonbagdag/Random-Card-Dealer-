48a16dd697e62d180fb5cbfc12a5d0ddd78c53ca
igcodinap <igcodinap@gmail.com>
2019-09-12 23:14:46 +0000

    Lógica de JS 1.0

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,31 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let pintas = Math.round(Math.random() * 3);
+  let num = Math.round(Math.random() * 13);
+  let valpintas = 0;
+
+  // Pinta de carta es corazon si pintas es 0
+  if (pintas === 0) {
+    valpintas === "&hearts;";
+  }
+  // Pinta de carta es pica si pintas es 1
+  if (pintas === 1) {
+    valpintas === "&spades;";
+  }
+  // Pinta de carta es trebol si pintas es 2
+  if (pintas === 2) {
+    valpintas === "&clubs;";
+  }
+  // Pinta de carta es diamante si pintas es 3
+  if (pintas === 3) {
+    valpintas === "&diams;";
+  }
+  //obtener el valor del ID y guardarlo en una variable
+  htmlpinta = document.getElementById("");
+  htmlnum = this.document.getElementById("");
+
+  //imprimir el valor de la variable en el HTML
+  htmlpinta.innerHTML = valpintas;
+  htmlnum.innerHTML = num;
 };
